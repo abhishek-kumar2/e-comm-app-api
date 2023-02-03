98cb4168eed4634fc9781d95ced967c3c055b5e5
Abhishek Kumar <abhishek2.kumar@trovicor.com>
2023-02-03 16:57:08 +0800

    eslint rc config with vscode plugins also

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+//https://velog.io/@kerem119/ESLint-Prettier-settings-for-in-Typescript-React
+module.exports = {
+    root: true,
+    extends: [
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+      'plugin:@typescript-eslint/eslint-recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+      'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    ],
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    env: {
+      browser: true,
+      es6: true,
+      jest: true,
+      node: true,
+    },
+    parserOptions: {
+      ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+      sourceType: 'module', // Allows for the use of imports
+      ecmaFeatures: {
+        arrowFunctions: true,
+      },
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.ts'],
+          paths: ['./src'],
+        },
+      },
+    },
+    rules: {
+      // Existing rules
+      'comma-dangle': 'off', // https://eslint.org/docs/rules/comma-dangle
+      'function-paren-newline': 'off', // https://eslint.org/docs/rules/function-paren-newline
+      'global-require': 'off', // https://eslint.org/docs/rules/global-require
+      'import/no-dynamic-require': 'off', // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-dynamic-require.md
+      'no-inner-declarations': 'off', // https://eslint.org/docs/rules/no-inner-declarations
+      // New rules
+      'class-methods-use-this': 'off',
+      'import/extensions': 'off',
+      'import/prefer-default-export': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+
+      'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+    },
+  };
